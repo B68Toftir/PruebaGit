b8cf1dddabb3c1c64c95b01d23cd228580dd67d3
Darth Aita <Darth Aita@DESKTOP-94892VF>
2023-05-04 09:27:57 +0200

    Version 0

---
+++ src/main/java/ed/ud04ejemplogit/Ud04ejemplogit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ed.ud04ejemplogit;
+
+/**
+ *
+ * @author Darth Aita
+ */
+public class Ud04ejemplogit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
